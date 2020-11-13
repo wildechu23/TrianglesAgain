3ecbbdeb0edfae466d408b94e891fcea036ebbae
Wilde Chu <wildechu23@gmail.com>
2020-11-13 01:07:08 -0500

    fixed distanceTo function, change - to +

--- Point.java
+++ Point.java
@@ -22,7 +22,7 @@ public class Point {
   public double distanceTo(Point other) {
     return Math.sqrt(
       Math.pow((this.x - other.x), 2) +
-      Math.pow((this.y + other.y), 2));
+      Math.pow((this.y - other.y), 2));
   }
   
   public boolean equals(Point other) {
